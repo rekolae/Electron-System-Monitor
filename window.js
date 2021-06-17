e67e612c421907424d7fb0a416a67147caa9cbc7
Emil Rekola <emil.rekola@hotmail.com>
2021-06-17 12:10:28 +0300

    Added 'fade-in-out' animation to the greeting text

--- window.js
+++ window.js
@@ -12,12 +12,59 @@ const greetings = [
     "namaste!",
     "buon giorno!",
     "zdravo!",
-    "Shalom!"
+    "Shalom!",
+    "Moi!"
 ]
 
+const randomizeGreeting = () => {
+
+    // Randomize greeting
+    let randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
+
+    // If same greeting was randomized -> pick the next one!
+    if (randomGreeting === greetingElement.textContent) {
+        newIndex = greetings.indexOf(randomGreeting) + 1
+        
+        // ... If out-of-bounds -> subtract two to pick the previous one!
+        if (newIndex >= greetings.length) {
+            newIndex -= 2
+        }
+
+        randomGreeting = greetings[newIndex];
+    }
+
+    return randomGreeting
+}
+
 const changeGreeting = () => {
-    const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
-    greetingElement.textContent = randomGreeting
+
+    // Ignore click events when animation is underway
+    if (greetingElement.classList.contains("is-changing")) {
+        return
+    }
+
+    // Start fade out animation
+    greetingElement.classList.add("is-changing")
+    greetingElement.classList.add("fade-text-out")
+}
+
+const fadeChange = () => {
+
+    // Text has faded out
+    if (greetingElement.classList.contains("fade-text-out")) {
+        greetingElement.classList.remove("fade-text-out")
+
+        // Change greeting text to a new one and start fade-in animation
+        greetingElement.textContent = randomizeGreeting()
+        greetingElement.classList.add("fade-text-in")
+    }
+
+    // Text has been faded in
+    else {
+        greetingElement.classList.remove("fade-text-in")
+        greetingElement.classList.remove("is-changing")
+    }
 }
 
 greetingElement.addEventListener("click", changeGreeting)
+greetingElement.addEventListener("animationend", fadeChange)
